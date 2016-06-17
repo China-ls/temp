229367cb46acca850111ba0fdbab039fe885e7c8
414057419@qq.com <hs6526342>
2016-06-17 17:35:09 +0800

    temp interfaces for framework

---
+++ framework/temp/temp-interfaces/src/main/java/com/infinite/framework/auth/util/AssertUtil.java
@@ -0,0 +1,14 @@
+package com.infinite.framework.auth.util;
+
+/**
+ * 使用Assert的方式来判定是否拥有权限
+ * @author by hx on 16-6-17.
+ * @since 1.0
+ */
+public abstract class AssertUtil {
+
+    public static void hasRole() {
+
+    }
+
+}
