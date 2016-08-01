0cdd129907f92eb1ab94b94b76e93b7fdec17054
414057419@qq.com <hs6526342>
2016-08-01 18:40:18 +0800

    修改DAO和Service

--- framework/temp/new-interface/src/main/java/com/infinite/framework/service/impl/VirtualSensorServiceImpl.java
+++ framework/temp/new-interface/src/main/java/com/infinite/framework/service/impl/VirtualSensorServiceImpl.java
@@ -1,16 +1,11 @@
 package com.infinite.framework.service.impl;
 
 import com.infinite.framework.core.persistent.IMongoDAO;
-import com.infinite.framework.entity.Application;
 import com.infinite.framework.entity.VirtualSensor;
 import com.infinite.framework.service.VirtualSensorService;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Filters;
-import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,39 +15,35 @@ import org.springframework.stereotype.Service;
 public class VirtualSensorServiceImpl implements VirtualSensorService {
     private static Logger log = LoggerFactory.getLogger(VirtualSensorServiceImpl.class);
 
-    @Value("#{dbConfig.vsensor_mongo_name_db}")
-    private String dbName;
-    @Value("#{dbConfig.vsensor_mongo_name_collection}")
-    private String collectionName;
-
     @Autowired
     private IMongoDAO mongoDAO;
 
     @Override
     public VirtualSensor createVSensor(/*String clientid,*/ String appid, VirtualSensor sensor) {
-        MongoCollection<Document> collection = mongoDAO.getCollection(dbName, collectionName);
-        Document documentApp = mongoDAO.findFirst(collection, Filters.eq("", appid));
-        if (null == documentApp) {
-            if (log.isDebugEnabled()) {
-                log.debug("should not create Virtual Sensor because appkey [{}] not exsist!", appid);
-            }
-            return null;
-        } else {
-            Application application = new Application();
-            application.fromDocument(documentApp);
-
-            application.addSensors();
-
-            Document findOneSensor = mongoDAO.findFirst(collection, Filters.eq("id", sensor.getId()));
-
-            if (null != findOneSensor) {
-                collection.findOneAndUpdate(Filters.eq("id", sensor.getId()), sensor.toDocument());
-                collection.findOneAndReplace(Filters.eq("id", sensor.getId()), sensor.toDocument());
-            } else {
-                collection.insertOne(sensor.toDocument());
-            }
-            return sensor;
-        }
+//        MongoCollection<Document> collection = mongoDAO.getCollection(dbName, collectionName);
+//        Document documentApp = mongoDAO.findFirst(collection, Filters.eq("", appid));
+//        if (null == documentApp) {
+//            if (log.isDebugEnabled()) {
+//                log.debug("should not create Virtual Sensor because appkey [{}] not exsist!", appid);
+//            }
+//            return null;
+//        } else {
+//            Application application = new Application();
+//            application.fromDocument(documentApp);
+//
+//            application.addSensors();
+//
+//            Document findOneSensor = mongoDAO.findFirst(collection, Filters.eq("id", sensor.getId()));
+//
+//            if (null != findOneSensor) {
+//                collection.findOneAndUpdate(Filters.eq("id", sensor.getId()), sensor.toDocument());
+//                collection.findOneAndReplace(Filters.eq("id", sensor.getId()), sensor.toDocument());
+//            } else {
+//                collection.insertOne(sensor.toDocument());
+//            }
+//            return sensor;
+//        }
+        return null;
     }
 
     @Override
